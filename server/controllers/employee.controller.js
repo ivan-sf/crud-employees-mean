46f8a50c8da78c6488b1f1844d11383db382bf43
Calvin <calvcastle666@gmail.com>
2018-06-21 00:47:21 -0500

    Rest API completa con Conexion a la Base de datos

--- server/controllers/employee.controller.js
+++ server/controllers/employee.controller.js
@@ -1,25 +1,40 @@
+const Employee = require('../models/employee.js');
 const employeeCtrl = {};
 
-employeeCtrl.getEmployees = (req, res) => {
+employeeCtrl.getEmployees = async (req, res) => {
+    const employees = await Employee.find();
+    res.json(employees);
+}
+
+employeeCtrl.createEmployee = async (req, res) =>{
+    const employee = new Employee(req.body);
+    await employee.save();    
     res.json({
-        status: 'Hola Isma API Campeon'
+        'status': 'Employee Saved'
     });
 }
 
-employeeCtrl.createEmployee = function(){
-    
+employeeCtrl.getEmployee = async (req, res) =>{
+    console.log();
+    const employee = await Employee.findById(req.params.id);
+    res.json(employee);
 }
 
-employeeCtrl.getEmployee = function(){
-    
+employeeCtrl.editEmployee = async (req,res) =>{
+    const { id } = req.params;
+    const employee = {
+        name: req.body.name,
+        position: req.body.position,
+        office: req.body.office,
+        salary: req.body.salary
+    };
+    await Employee.findByIdAndUpdate(id, {$set: employee}, {new:true});
+    res.json({status: "Employee Update"});
 }
 
-employeeCtrl.editEmployee = function(){
-    
-}
-
-employeeCtrl.deleteEmployee = function(){
-    
+employeeCtrl.deleteEmployee = async (req,res) =>{
+    await Employee.findByIdAndRemove(req.params.id);
+    res.json({status:'Employee deleted'});
 }
 
 module.exports = employeeCtrl;
